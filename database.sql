2e6b7783f860d670d871805571b7d2db205727ae
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-09-07 15:44:39 -0500

    update database

--- database.sql
+++ database.sql
@@ -5,6 +5,25 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) NOT NULL,
     "username" VARCHAR (80) UNIQUE NOT NULL,
+    "email" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "brewery" (
+	"id" SERIAL PRIMARY KEY,
+	"brewery" VARCHAR(80),
+	"location_api" VARCHAR(256)
+);
+
+CREATE TABLE "ratings" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" NOT NULL,
+	"brewery_id" INT REFERENCES "brewery" NOT NULL,
+	"beer" VARCHAR(80),
+	"type" VARCHAR(25),
+	"rating" DECIMAL(5,2),
+	"notes" VARCHAR(256),
+	"date" DATE
 );
