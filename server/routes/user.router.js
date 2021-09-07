3057c52f634b1c9c5907b0f8740cdf90fa6dc71a
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-09-07 15:11:49 -0500

    Add name, email to registerForm

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -18,13 +18,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
+  const name = req.body.name;
   const username = req.body.username;
+  const email = req.body.email;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (name, username, email, password)
+    VALUES ($1, $2, $3, $4) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [name, username, email, password])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
