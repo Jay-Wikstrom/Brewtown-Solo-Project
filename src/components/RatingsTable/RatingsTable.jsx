87980c64327b3f38abc17390ac34e87c50121037
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-09-08 16:33:29 -0500

    Code cleanup

--- src/components/RatingsTable/RatingsTable.jsx
+++ src/components/RatingsTable/RatingsTable.jsx
@@ -19,7 +19,6 @@ function RatingsPage(){
         
         <div>
             <h1>This will be my ratings table</h1>
-            
 
             <table>
                 <thead>
@@ -63,62 +62,7 @@ function RatingsPage(){
                         );
                     })}
                 </tbody>
-
-
-                <tbody>
-                    <td>
-                        <tr>Surly</tr>
-                    </td>
-                    <td>
-                        <tr>Furious</tr>
-                    </td>
-                    <td>
-                        <tr>IPA</tr>
-                    </td>
-                    <td>
-                        <tr>4.5</tr>
-                    </td>
-                    <td>
-                        <tr>My first rating</tr>
-                    </td>
-                    <td>
-                        <tr>09/04/21</tr>
-                    </td>
-                    <td>
-                        <tr>Delete</tr>
-                    </td>
-                </tbody>
-
-                <tbody>
-                    <td>
-                        <tr>Indeed</tr>
-                    </td>
-                    <td>
-                        <tr>Flavorwave</tr>
-                    </td>
-                    <td>
-                        <tr>IPA</tr>
-                    </td>
-                    <td>
-                        <tr>3.2</tr>
-                    </td>
-                    <td>
-                        <tr>A bit to pineappley</tr>
-                    </td>
-                    <td>
-                        <tr>09/04/21</tr>
-                    </td>
-                    <td>
-                        <tr>Delete</tr>
-                    </td>
-                </tbody>
-
             </table>
-
-         
-            
-            {/* {ratings.map(rating => (
-                <p key={rating.id}>Brewery: {rating.brewery} Image Url: {items.image_url}></p> */}
             
         </div>
     )
