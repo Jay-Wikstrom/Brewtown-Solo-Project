f1e2da99546d74f8275d15b44aa7170d50d175e8
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-09-09 18:30:21 -0500

    more cleanup

--- src/components/BeerRatingPage/BeerRatingPage.jsx
+++ src/components/BeerRatingPage/BeerRatingPage.jsx
@@ -3,27 +3,7 @@ import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
 function BeerRatingPage() {
-    // useEffect(() => {
-    //     console.log(location.pathname); // result: '/secondpage'
-    //     //console.log(location.state.detail); // result: 'some_value'
-    // }, [location]);
-
-    // useEffect(() => {
-    //     if (state) {
-    //         history.push('/beer-list', { state });
-    //     }
-    // }, [state]);
-
-    // const location = useLocation();
-
-    // console.log(location);
-    // console.log(location.state);
-
-    // // location.state would be undefined if user is directly taking this url
-    // const { state } = location.state || { searchInput: '' };
-
     const breweryReducer = useSelector(store => store.breweryReducer);
-    
 
     const dispatch = useDispatch();
 
@@ -36,7 +16,6 @@ function BeerRatingPage() {
     return (
         <div>
             <h1>This will be my Beer Rating Page</h1>
-
 
             <input 
                 type='text'
@@ -54,31 +33,6 @@ function BeerRatingPage() {
                 type='text'
                 placeholder='Notes'
             />
-
-            {/* <table>
-                <thead>
-                    <th>Beer</th>
-                    <th>Rating</th>
-                    <th>Type</th>
-                    <th>Notes</th>
-                </thead>
-
-                <tbody>
-                    
-                    <td>
-                        <tr>Furious</tr>
-                    </td>
-                    <td>
-                        <tr>IPA</tr>
-                    </td>
-                    <td>
-                        <tr>4.5</tr>
-                    </td>
-                    <td>
-                        <tr>My First Beer</tr>
-                    </td>
-                </tbody>
-            </table> */}
         </div>
     )
 }
