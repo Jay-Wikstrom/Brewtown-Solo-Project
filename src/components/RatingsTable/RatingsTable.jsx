db9af1b283d9e07a8bf3b9eb7b65feb080d5e6bd
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-09-10 12:11:07 -0500

    Code Cleanup

--- src/components/RatingsTable/RatingsTable.jsx
+++ src/components/RatingsTable/RatingsTable.jsx
@@ -1,6 +1,5 @@
 import { Paper, TableBody, TableCell, TableSortLabel } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
-import { NavItem } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { TableRow, TableHead, Table, Button, makeStyles } from "@material-ui/core";
 
@@ -8,6 +7,20 @@ import { TableRow, TableHead, Table, Button, makeStyles } from "@material-ui/cor
 function RatingsPage(){
     const dispatch = useDispatch();
     const ratings = useSelector(store => store.ratingsReducer);
+
+    const pages = [5, 10, 25]
+    const [page, setPages] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5)
+    const [order, setOrder] = useState();
+    const [orderBy, setOrderBy] = useState()
+
+    const headCells = [
+        { id: 'brewery', label: 'Brewery' },
+        { id: 'beer', label: 'Beer' },
+        { id: 'type', label: 'Type' },
+        { id: 'ratings', label: 'Ratings' },
+        { id: 'notes', label: 'Notes' },
+    ]
 
     useEffect(()=>{
         dispatch({
@@ -28,8 +41,8 @@ function RatingsPage(){
             marginTop: theme.spacing(3),
             '& thead th': {
                 fontWeight: '600',
-                color: theme.palette.primary.main,
-                backgroundColor: theme.palette.primary.light,
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.success.light,
             },
             '& tbody td': {
                 fontWeight: '300',
@@ -42,25 +55,13 @@ function RatingsPage(){
         }
     }))
     const classes = useStyles();
-    const pages = [5, 10, 25]
-    const [page, setPages] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5)
-    const [order, setOrder] = useState();
-    const [orderBy, setOrderBy] = useState()
+    
 
     const handleSort = cellId => {
         const isAsc = orderBy === cellId && order === "asc";
         setOrder(isAsc ? 'desc' : 'asc');
         setOrderBy(cellId)
     }
-
-    const headCells = [
-        { id: 'brewery', label: 'Brewery'},
-        { id: 'beer', label: 'Beer' },
-        { id: 'type', label: 'Type' },
-        { id: 'ratings', label: 'Ratings' },
-        { id: 'notes', label: 'Notes' },
-    ]
 
     function sort(array, comparator){
         const stabilizedThis = array.map((el, index) => [el, index]);
@@ -93,15 +94,12 @@ function RatingsPage(){
     }
 
     return (
-        
         <div>
             <h1>Brews Rated</h1>
-
             <Paper>
                 <Table className={classes.table}>
                     <TableHead>
                        <TableRow>
-                            
                             {
                                 headCells.map(headCell => (
                                     <TableCell key={headCell.id}
@@ -114,38 +112,13 @@ function RatingsPage(){
                                             {headCell.label}
                                         </TableSortLabel>
                                     </TableCell>
-
-                                    
                                 ))
                             }
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            {/* <TableCell>
-                                <TableSortLabel>Brewery</TableSortLabel>
-                            </TableCell>
-                            <TableCell>
-                                <TableSortLabel>Beer</TableSortLabel>
-                            </TableCell>
-                            <TableCell>
-                                <TableSortLabel>Type</TableSortLabel>
-                            </TableCell>
-                            <TableCell>
-                                <TableSortLabel>Rating</TableSortLabel>
-                            </TableCell>
-                            <TableCell>
-                                <TableSortLabel>Notes</TableSortLabel>
-                            </TableCell> */}
                        </TableRow>
                     </TableHead>
                     <TableBody>
                         {
                             afterSort().map(rating =>
-
                             (<TableRow key={rating.id}>
                                 <TableCell>{rating.brewery}</TableCell>
                                 <TableCell>{rating.beer}</TableCell>
@@ -167,51 +140,6 @@ function RatingsPage(){
                     </TableBody>
                 </Table>
             </Paper>
-
-            {/* <table>
-                <thead>
-                    <th>Brewery</th>
-                    <th>Beer</th>
-                    <th>Type</th>
-                    <th>Rating</th>
-                    <th>Notes</th>
-                    <th>Date</th>
-                </thead>
-
-
-                <tbody>
-                    {ratings.map((rating) => {
-                        return (
-                            <tr key={rating.id}>
-                                <td>
-                                    {rating.brewery}
-                                </td>
-                                <td>
-                                    {rating.beer}
-                                </td>
-                                <td>
-                                    {rating.type}
-                                </td>
-                                
-                                <td>
-                                    {rating.rating}
-                                </td>
-
-                                <td>
-                                    {rating.notes}
-                                </td>
-                                <td>
-                                    Date
-                                </td>
-                                <td>
-                                    <button onClick={() => handleDelete(rating.id)}>Delete</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table> */}
-            
         </div>
     )
 }
